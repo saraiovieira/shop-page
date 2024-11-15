a9d4239eace2cb145e5db546d7b6b1f7aff9b6a9
saraiovieira <saraiovieira99@gmail.com>
2024-11-15 12:55:26 +0000

    fetch data from the api and design the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
-const Home = () => {
-  return <div></div>;
+import ProductCard from "@/components/productCard/productCard";
+import styles from "./home.module.css";
+const { API_URL } = process.env;
+
+const getData = async () => {
+  const res = await fetch(`${API_URL}`, {cache: "no-store"});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const Home = async () => {
+  const posts = await getData();
+
+  return (
+    <main className={styles.container}>
+      {posts.map((post) => (
+        <ProductCard post={post} key={post.id}/>
+      ))}
+
+ 
+    </main>
+  );
 };
 
 export default Home;
