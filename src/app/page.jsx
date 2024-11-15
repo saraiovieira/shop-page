31d8a8404fbfd1d5208d4fe4e364973d1af323ee
saraiovieira <saraiovieira99@gmail.com>
2024-11-15 23:14:35 +0000

    change width and names

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./home.module.css";
 const { API_URL } = process.env;
 
 const getData = async () => {
-  const res = await fetch(`${API_URL}`, {cache: "no-store"});
+  const res = await fetch(`${API_URL}`, { cache: "no-store" });
 
   if (!res.ok) {
     throw new Error("Something went wrong");
@@ -13,15 +13,13 @@ const getData = async () => {
 };
 
 const Home = async () => {
-  const posts = await getData();
+  const products = await getData();
 
   return (
     <main className={styles.container}>
-      {posts.map((post) => (
-        <ProductCard post={post} key={post.id}/>
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
       ))}
-
- 
     </main>
   );
 };
